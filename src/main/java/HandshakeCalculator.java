c14668e599ad06981bec7e7b1e06bfc41d2e15c5
naveencoder <naveenge42@gmail.com>
2019-07-26 00:14:15 +0530

    first commit

---
+++ src/main/java/HandshakeCalculator.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+class HandshakeCalculator {
+
+    List<Signal> calculateHandshake(int number) {
+        Signal signals[] = {
+                Signal.WINK,
+                Signal.DOUBLE_BLINK,
+                Signal.CLOSE_YOUR_EYES,
+                Signal.JUMP
+        };
+
+        int reverse = 16;
+        List<Signal> result = new ArrayList<Signal>();
+
+        for (Signal s : signals) {
+            int mask = 1 << s.ordinal();
+            if ((mask & number) > 0) {
+                result.add(s);
+            }
+        }
+
+        if ((reverse & number) > 0) {
+            Collections.reverse(result);
+        }
+
+        return result;
+    }
+}
+
+
